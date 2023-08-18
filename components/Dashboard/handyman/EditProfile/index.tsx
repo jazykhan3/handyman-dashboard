7a6e3029e4ae262f85ee41be257d5f777ddbc11b
jahanzaib sultan <jazykhan7@gmail.com>
2023-08-18 23:40:12 +0500

    fix lint issues

--- components/Dashboard/handyman/EditProfile/index.tsx
+++ components/Dashboard/handyman/EditProfile/index.tsx
@@ -67,7 +67,7 @@ export default function Index() {
         {" "}
         <p>
           Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          industry. Lorem Ipsum has been the industry&apos;s standard dummy text ever
           since the 1500s, when an unknown printer took a galley of type and
           scrambled it to make a type specimen book. It has survived not only
           five centuries, but also the leap into electronic typesetting,
@@ -81,7 +81,7 @@ export default function Index() {
         </section>
         <section className="flex flex-row gap-20">
         {handymanPofileImgsSrc?.map((imgSrc, idx) => (
-  <img src={imgSrc} alt="" key={idx} />
+  <Image src={imgSrc} alt="" key={idx} />
 ))}
 
         </section>
